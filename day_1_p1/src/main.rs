621f66c802baf826201a894fbc0415c8fc4a14eb
Paul Lane <paul.lane083@gmail.com>
2021-12-02 17:50:20 -0500

    Optimizing day 1 part 1 and 2

--- day_1_p1/src/main.rs
+++ day_1_p1/src/main.rs
@@ -1,30 +1,10 @@
-use std::env;
-use std::fs::File;
-use std::io::Read;
+use std::str;
 
 fn main() {
-    let args = env::args().collect::<Vec<_>>();
-    if args.len() < 2 {
-        println!("Supply puzzle input file as first parameter");
-        std::process::exit(1);
-    }
-    let file_name = args[1].clone();
+    let input =
+        str::from_utf8(include_bytes!("../input/input.txt")).expect("File is not valid UTF-8");
 
-    let mut input_file = match File::open(&file_name) {
-        Ok(file) => file,
-        Err(err) => {
-            println!("Could not open input file \"{}\": {}", file_name, err);
-            std::process::exit(1);
-        }
-    };
-
-    let mut input_contents = String::new();
-    if let Err(err) = input_file.read_to_string(&mut input_contents) {
-        println!("Could not read input file \"{}\": {}", file_name, err);
-        std::process::exit(1);
-    }
-
-    let sum: usize = input_contents
+    let sum: usize = input
         .lines()
         .map(|line| {
             line.parse::<i32>()
